6d72ba5b90ec70dc37ce6b8f59ffbe95a109cf09
Casey Schneider <caseyschneider77@gmail.com>
2026-07-05 18:46:39 -0700

    Fix delete-student import statement
    Signed-off-by: Casey Schneider <caseyschneider77@gmail.com>

--- app/routes/students/delete-student.tsx
+++ app/routes/students/delete-student.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "react-router"
-import type { Route } from "./+types/delete"
+import type { Route } from "./+types/delete-student"
 
 export async function action({ params }: Route.ClientLoaderArgs) {
   await fetch(`http://localhost:3000/api/v1/students/${params.studentId}`, {
